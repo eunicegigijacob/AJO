6e76d62bf8dcb04330f357c1f7fdd8cda4a367bf
EUNICE JACOB <115852649+eunicegigijacob@users.noreply.github.com>
2023-02-22 17:26:20 +0100

    install cookie parser, jwt, and other dependencies

--- Backend folder/app.js
+++ Backend folder/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const { routemanager } = require('./routes/routes');
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cookieParser());
+
+app.use('/', routemanager);
 
 app.get('/health', (req, res) => {
   res.status(200).json('Server up!');

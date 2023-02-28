084e359cae39f56fe7946c94732708a2a8352e25
EUNICE JACOB <115852649+eunicegigijacob@users.noreply.github.com>
2023-02-28 03:10:39 +0100

    instals and use cors package for resource sharing

--- Backend folder/app.js
+++ Backend folder/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const { routemanager } = require('./routes/routes');
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));

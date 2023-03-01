d49a6b7ca85e30f61be958fc0eb401b1f77e860f
EUNICE JACOB <115852649+eunicegigijacob@users.noreply.github.com>
2023-03-01 11:28:42 +0100

    use mongodb atlass connection.

--- Backend folder/db.js
+++ Backend folder/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 const connectDB = async () => {
   mongoose.set('strictQuery', false);
   mongoose
-    .connect(process.env.MONGO_URL, {
+    .connect(process.env.DB_URL, {
       useNewUrlParser: true,
     })
     .then(() => console.log('Database connected successfully'))

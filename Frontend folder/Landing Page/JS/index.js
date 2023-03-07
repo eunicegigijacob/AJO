d9df37695f600af4f06277f6e715902ab1ea4773
EUNICE JACOB <115852649+eunicegigijacob@users.noreply.github.com>
2023-03-07 11:53:52 +0100

    link siignup and signIN pages

--- Frontend folder/Landing Page/JS/index.js
+++ Frontend folder/Landing Page/JS/index.js
@@ -1,7 +1,17 @@
-let signinBtn = document.getElementById(signinBtn);
-let signupBtn = document.getElementById(signunBtn);
-let aboutUs = document.getElementById(aboutUs);
+const signinBtn = document.getElementById('signinBtn');
+const signupBtn = document.getElementById('signupBtn');
+const aboutUs = document.getElementById('aboutUs');
 
-function signin(){
-    window.location.assign("../Registration Screen/HTML/signin.html")
-}
+signinBtn.addEventListener('click', () => {
+  location.assign('Pages/signin.html');
+  console.log('signin');
+});
+
+signupBtn.addEventListener('click', () => {
+  location.assign('Pages/signup.html');
+  console.log('signup');
+});
+
+// function signin(){
+//    location.assign("/signin.html")
+// }

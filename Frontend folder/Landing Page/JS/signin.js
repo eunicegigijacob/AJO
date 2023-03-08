9194fad50ed695dee559a4fd8c54f31ab7583ab1
Oputa Olivia Amarachi <72948572+oputaolivia@users.noreply.github.com>
2023-03-08 14:57:32 +0000

    Login linked

--- Frontend folder/Landing Page/JS/signin.js
+++ Frontend folder/Landing Page/JS/signin.js
@@ -1,13 +1,29 @@
-function signin(){
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-    if(email== "admin@gmail.com" && password=="admin123"){
-        window.location.assign("../Landing Page/HTML/index.html");
-        alert("Login Successful");
+const datas = {
+        email: document.getElementById("email"),
+        password: document.getElementById("password"),
+        form: document.querySelector("form"),
+    };
+    console.log(datas);
+datas.form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    let {email, password} = datas;
+    let inputs = {
+        email: email.value,
+        password: password.value,
     }
-    else{
-        alert("Invalid Information");
-        return;
-    }
-}
+    senData("https://ajo.onrender.com/api/v1/auth/login", inputs);
+    email.value = password.value = "";
+});
 
+const senData = async (url, inputs) =>{
+    const response = await fetch(url, {
+        headers: {
+      'Content-Type': 'application/json'},
+        method: "POST",
+        body: JSON.stringify(inputs),
+      });
+      const data = response.status;
+      if (data === 200){
+        window.location.href = "../Pages/dashboard.html"
+      }
+}

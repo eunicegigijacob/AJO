3ee859c86b71cb04000b70b3166a520acf901c22
EUNICE JACOB <115852649+eunicegigijacob@users.noreply.github.com>
2023-03-10 15:10:50 +0100

    Update db.js

--- Backend folder/db.js
+++ Backend folder/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 const connectDB = async () => {
   mongoose.set('strictQuery', false);
   mongoose
-    .connect(process.env.MONGO_URL)
+    .connect(process.env.DB_URL)
     .then(() => console.log('Database connected successfully'))
     .catch((err) => {
       console.log(err);
